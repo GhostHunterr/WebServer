385fade6c37af7d194133c9d231ef311471d1882
Farhan <farhansarang7@gmail.com>
2025-02-21 03:44:23 -0500

    Implemented Single Threaded Server.

--- SingleThreaded/Server.java
+++ SingleThreaded/Server.java
@@ -1,6 +1,45 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
+    public void run() throws IOException {
+        int port = 8010;
+        ServerSocket socket = new ServerSocket(port);
+        socket.setSoTimeout(10000);
+        while (true) {
+            try {
+                System.out.println("Server is Listening on port: " + port);
+                Socket acceptedConnection = socket.accept();
+                System.out.println("Connection accepted from client" + acceptedConnection.getRemoteSocketAddress());
+                PrintWriter toClient = new PrintWriter(acceptedConnection.getOutputStream(), true);
+                BufferedReader fromClient = new BufferedReader(new InputStreamReader(acceptedConnection.getInputStream()));
+
+                toClient.println("Hello From the Server!");
+                String line = fromClient.readLine();
+                System.out.println("Message from the Client: " + line);
+
+                acceptedConnection.close();
+                toClient.close();
+                fromClient.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("hello");
+        try {
+            Server server = new Server();
+            server.run();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
